6f5f847b25900ea5f7cc787db06cc6a8be903cb2
Mbals-dot <Mskhosana19@gmail.com>
2025-01-01 14:50:50 -0800

    0x06-regular_expressions

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+# Check if exactly one argument is passed
+if ARGV.length == 1
+  input = ARGV[0]
+
+  # Regular expression to match 'School' using Oniguruma syntax
+  if input.match?(/School\z/)
+    puts "Match found: #{input}"
+  else
+    puts "No match found"
+  end
+else
+  puts "Please provide one argument."
+end
